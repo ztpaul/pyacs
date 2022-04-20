622aa7bbb5a6ee638b91bcd7c931e3ffea2a0b67
Edward <ballzb@qq.com>
2022-04-20 23:17:21 +0800

    modify html

--- templates/web/index.js
+++ templates/web/index.js
@@ -1,5 +1,28 @@
-function OnSubmit() {
+window.onload=function()
+{ 
+    document.getElementById('selectMethod').onchange(this.value);
+}
+
+function OnSubmit()
+{
     window.history.replaceState(null, null, window.location.href);
     return true
- }
+}
+
+
+
+function OnChangeMethod(value)
+{
+    if(value=="SetParameterValues")
+    {
+        document.getElementById('tdValue').style.display = "";
+        document.getElementById('tdInputValue').style.display = "";
+    }
+    else
+    {
+        document.getElementById('tdValue').style.display = "none";
+        document.getElementById('tdInputValue').style.display = "none";
+    }
+    //alert(value)
+}
  
